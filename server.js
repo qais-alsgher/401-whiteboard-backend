c3f607ce0fd81ceeb79df73d5dc1b0135b62f8ff
qais-alsgher <qaisalsgher@gmail.com>
2022-09-12 13:24:10 +0300

    crud sestem for message wihout test

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const messageRuoter = require('./routes/post.route');
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(messageRuoter);
 
 app.get('/', (req, res) => {
     res.status(200).json({
-        message: "qais"
+        message: "hello word"
     })
 })
+
 function start(port) {
     app.listen(port, () => { console.log(`the server start for port ${port}`) });
 }
